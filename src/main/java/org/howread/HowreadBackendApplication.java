36985a10f545b5edd98267096367d06e3f4aa58b
ghktndyd <hwasuyong99@naver.com>
2026-03-19 16:04:33 +0900

    first commit

---
+++ src/main/java/org/howread/HowreadBackendApplication.java
@@ -0,0 +1,13 @@
+package org.howread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowreadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowreadBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/howread/HowreadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.howread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowreadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
